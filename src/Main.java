6d85b10c4fd84401660edbf56e1cc3e2aa1bbaae
Julia Xie <juliaxie18020@gmail.com>
2021-10-27 22:15:40 -0400

    added another xml file

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import javax.sound.sampled.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        File file = new File("MusicXML/Reprise.musicxml");
+        File file = new File("MusicXML/One_Summer's_Day.musicxml");
         File output = new File ("aaaa.midi");
         MusicXMLFileHelper.xml2midi(file,output);
     }
