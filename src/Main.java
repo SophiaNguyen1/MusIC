c2e2a111f628660a607c220b776391a5a5848ea6
Julia Xie <juliaxie18020@gmail.com>
2021-10-27 22:38:01 -0400

    fix http error (so i could get rid of musicxml stuff)

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        System.setProperty("http.agent", "Mozilla/5.0 (X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0");
         File file = new File("MusicXML/Bohemian_Rhapsody.musicxml");
         File output = new File ("aaaa.mid");
         MusicXMLFileHelper.xml2midi(file,output);
