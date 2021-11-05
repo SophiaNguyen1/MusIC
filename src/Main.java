f0297ac1676335131edb8064dfcf342631fd5ab3
Julia Xie <juliaxie18020@gmail.com>
2021-11-05 12:15:16 -0400

    added in a version of portals that works

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 /* Where the program breaks:
  * - multiple voices starting at different spots
  * - time signatures that aren't 4/4 (just plays at the wrong tempo)
- * - uneven subdivisions of notes (crazy tuplets and whatever)
+ * - uneven subdivisions of notes with a lot of subdivision (9 tuplets don't work but 6 and under work)
  */
 
 public class Main {
@@ -11,6 +11,6 @@ public class Main {
         System.setProperty("http.agent", "Mozilla/5.0 (X11; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0");
         File br = new File("MusicXML/Bohemian_Rhapsody.musicxml");
         File output = new File ("aaaa.wav");
-        MusicXMLFileHelper.xml2midi(br,output);
+        MusicXMLFileHelper.xml2midi(MusicXMLFileHelper.xmlEdit(new File ("MusicXML/Portals.musicxml")),output);
     }
 }
